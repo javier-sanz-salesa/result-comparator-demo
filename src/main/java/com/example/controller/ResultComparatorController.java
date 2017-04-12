36dbfc3816b76b1dbd0890bf303012b5f164c721
Javier Sanz <jsanz@autentia.com>
2017-04-12 08:57:52 +0200

    Initial commit

---
+++ src/main/java/com/example/controller/ResultComparatorController.java
@@ -0,0 +1,7 @@
+package com.example.controller;
+
+/**
+ * Created by javier.sanz.salesa on 11/04/2017.
+ */
+public class ResultComparatorController {
+}
---
+++ src/main/java/com/example/service/RestServiceCaller.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+/**
+ * Created by javier.sanz.salesa on 11/04/2017.
+ */
+public interface RestServiceCaller {
+}
