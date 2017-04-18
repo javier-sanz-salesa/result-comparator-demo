5c5a74b90086bf3d036796dc5e1ff35ee3fe11d0
Javier Sanz <jsanz@autentia.com>
2017-04-18 08:10:32 +0200

    Add some changes to do a POC and download on isolated corporate environment

--- src/main/java/com/example/controller/ResultComparatorController.java
+++ src/main/java/com/example/controller/ResultComparatorController.java
@@ -2,10 +2,15 @@ package com.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Rest controller that provides the application endpoints
@@ -20,7 +25,20 @@ public class ResultComparatorController {
     public void compare(@RequestParam(name = "personId") String personId,
                         @RequestParam(name = "nif") String nif,
                         @RequestParam(name = "birthDate") String birthDate) {
-        logger.info("Aquí estamos con info");
-        logger.debug("Aquí estamos con debug");
+        AsyncRestTemplate asyncRestTemplate1 = new AsyncRestTemplate();
+        String url = "http://www.google.es";
+        HttpMethod method = HttpMethod.GET;
+        Class<String> responseType = String.class;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> requestEntity = new HttpEntity<>("params", headers);
+        ListenableFuture<ResponseEntity<String>> future = asyncRestTemplate1.exchange(url, method, requestEntity, responseType);
+
+        try {
+            ResponseEntity<String> entity = future.get();
+            System.out.println("entity.getBody() = " + entity.getBody());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
